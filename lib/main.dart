c7a0242abdb9922557b15e124c877f3f0cf1a597
s-yoshino <shou-y6216@yoshinoda.com>
2020-09-01 01:00:38 +0900

    [update] ListViewを表示しました

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,147 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  List<Map<String, dynamic>> _listItems = [
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    },
+    {
+      'no': '1',
+      'name': 'スライム',
+      'kill_rainbow': true,
+      'heart_rank_s': true
+    }
+  ];
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -72,51 +213,21 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:!!!',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-              textAlign: TextAlign.left
-            ),
-            Text(
-              'AAAAA',
-              style: TextStyle(fontWeight: FontWeight.bold)
-            )
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: ListView.builder(
+            itemCount: _listItems.length,
+            itemBuilder: (context, index) {
+              return Card(
+                  child: ListTile(
+                    title: Text(_listItems[index]['no']),
+                  )
+              );
+            }
+        )
     );
   }
 }
